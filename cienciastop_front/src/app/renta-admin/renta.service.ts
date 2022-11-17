9a960ccb52e05d37cee636641ca3aee9d354099a
Adriana <ady_gasca@ciencias.unam.mx>
2022-11-16 18:28:30 -0600

    Excepciones listas, redireccion sin funcionar

--- cienciastop_front/src/app/renta-admin/renta.service.ts
+++ cienciastop_front/src/app/renta-admin/renta.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { of } from 'rxjs';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Renta } from './renta';
+import { catchError } from 'rxjs';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +14,20 @@ export class RentaService {
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   private urlEndPoint:string = 'http://localhost:8080/api/rentas';
   getRentas(): Observable<Renta[]> {
     return this.http.get<Renta[]>(this.urlEndPoint);
   }
   update(id: number): Observable<Renta>{
-    return this.http.put<Renta>( `${this.urlEndPoint}/${id}`, {headers: this.httpHeaders});
+    return this.http.put<Renta>( `${this.urlEndPoint}/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(e =>{
+        this.router.navigate(['/renta-admin'])
+        Swal.fire('Error al actualizar producto', e.error.mensaje, "error");
+        return throwError( () => e);
+        
+      })
+    );
   }
 }
